f0b57d751aab9ff4c386c46759ef268242f45014
artemmatiushenko1 <artom.matyushenko@gmail.com>
2025-07-04 08:34:31 +0300

    add auth banner img

--- apps/web/src/app/auth/page.tsx
+++ apps/web/src/app/auth/page.tsx
@@ -1,17 +1,32 @@
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
+import Image from 'next/image';
 import React from 'react';
 
 function Auth() {
   return (
     <div className="flex h-dvh">
       <div className="flex-2/3 p-5 h-full">
-        <div className="h-full rounded-2xl bg-gray-400">Picture</div>
+        <div className="h-full rounded-2xl bg-gray-200 relative overflow-hidden">
+          <Image
+            alt="3d camera render"
+            className="object-cover object-center"
+            src="/auth-banner.jpg"
+            fill
+          />
+        </div>
       </div>
-      <div className="flex-1/2">
+      <div className="flex-1/2 p-5">
         <div className="w-md m-auto">
-          <div className="text-center mt-40 mb-15">
+          <Image
+            src="/logo.png"
+            width={50}
+            height={50}
+            alt="logo"
+            className="rounded-md m-auto mt-20"
+          />
+          <div className="text-center mt-15 mb-15">
             <h1 className="text-3xl font-bold">Welcome Back</h1>
             <p>Enter your email and password to access your account</p>
           </div>
@@ -35,7 +50,7 @@ function Auth() {
             <Button className="w-full mb-6">Sign In</Button>
           </form>
           <div className="w-full h-[1px] bg-gray-200 relative mb-6">
-            <div className="absolute left-1/2 bottom-1/2 leading-1 bg-white text-gray-200 size-5 translate-y-1/2 pb-1 flex items-center justify-center">
+            <div className="absolute left-1/2 bottom-1/2 leading-1 bg-white text-gray-300 size-5 translate-y-1/2 -translate-x-1/2 pb-1 flex items-center justify-center">
               or
             </div>
           </div>
