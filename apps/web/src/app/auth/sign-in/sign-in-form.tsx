270055e429fe59e86519c003c4525a41dd46513e
artemmatiushenko1 <artom.matyushenko@gmail.com>
2025-07-05 21:12:00 +0300

    add link. to sign up

--- apps/web/src/app/auth/sign-in/sign-in-form.tsx
+++ apps/web/src/app/auth/sign-in/sign-in-form.tsx
@@ -3,6 +3,7 @@ import { Logo } from '@/components/logo';
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
+import Link from 'next/link';
 
 const SignInForm = () => {
   return (
@@ -12,9 +13,7 @@ const SignInForm = () => {
       </div>
       <div className="text-center mt-10 mb-15">
         <h1 className="text-3xl font-bold">Welcome Back</h1>
-        <p className="text-sm">
-          Enter your email and password to access your account
-        </p>
+        <p className="text-sm">Enter your credentials to access your account</p>
       </div>
       <form>
         <div className="flex flex-col gap-3 mb-6">
@@ -29,18 +28,25 @@ const SignInForm = () => {
             placeholder="Enter your password"
           />
         </div>
-        <Button className="w-full mb-6 font-bold" size="lg">
+        <Button className="w-full mb-8 font-bold" size="lg">
           Sign In
         </Button>
       </form>
-      <div className="w-full h-[1px] bg-gray-200 relative mb-6">
-        <div className="absolute text-sm left-1/2 bottom-1/2 leading-1 bg-white text-black size-8 translate-y-1/2 -translate-x-1/2 p-1 flex items-center justify-center">
+      <div className="w-full h-[1px] bg-gray-200 relative mb-8">
+        <div className="absolute text-sm left-1/2 bottom-1/2 leading-1 bg-background text-foreground size-8 translate-y-1/2 -translate-x-1/2 p-1 flex items-center justify-center">
           Or
         </div>
       </div>
-      <Button variant="outline" className="w-full" size="lg">
+      <Button variant="outline" className="w-full mb-8" size="lg">
         <GoogleIcon /> Sign In with Google
       </Button>
+
+      <p className="text-sm text-center text-muted-foreground">
+        Don&apos;t have an account?{' '}
+        <Link href="/auth/sign-up" className="font-bold text-foreground">
+          Sign Up
+        </Link>
+      </p>
     </div>
   );
 };
