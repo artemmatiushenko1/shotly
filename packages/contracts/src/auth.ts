a7c1ab9e69dfe671af409e304d876a8d242ea474
artemmatiushenko1 <artom.matyushenko@gmail.com>
2025-07-06 15:47:19 +0300

    install tsrest for all apps

--- packages/contracts/src/auth.ts
+++ packages/contracts/src/auth.ts
@@ -1,22 +1,5 @@
 import { initContract } from '@ts-rest/core';
-import { z } from 'zod';
 
 const c = initContract();
 
-export const contract = c.router({
-  searchPokemon: {
-    method: 'GET',
-    path: '/pokemons',
-    query: z.object({
-      name: z.string().optional(),
-      type: z.string().optional(),
-    }),
-    responses: {
-      200: z.object({
-        name: z.string(),
-        type: z.string(),
-      }),
-    },
-    summary: 'Search for a pokemon',
-  },
-});
+export const contract = c.router({});
