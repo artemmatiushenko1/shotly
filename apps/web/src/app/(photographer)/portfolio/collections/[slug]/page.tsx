2c2b9d5b4282a860439d98c4bcb7d3aeebc8ea46
artemmatiushenko1 <artom.matyushenko@gmail.com>
2025-07-21 22:27:02 +0300

    remake details page

--- apps/web/src/app/(photographer)/portfolio/collections/[slug]/page.tsx
+++ apps/web/src/app/(photographer)/portfolio/collections/[slug]/page.tsx
@@ -1,22 +1,18 @@
-import MainHeader from '@/components/main-header';
 import { Badge } from '@shotly/ui/components/badge';
 import { Button, buttonVariants } from '@shotly/ui/components/button';
-import { Separator } from '@shotly/ui/components/separator';
 import { cn } from '@shotly/ui/lib/utils';
 import {
+  CalendarIcon,
   ChevronLeft,
+  EllipsisIcon,
   Globe,
-  GridIcon,
   ImagesIcon,
-  ListIcon,
   Lock,
-  UploadIcon,
+  MapPinIcon,
+  TagIcon,
 } from 'lucide-react';
 import Image from 'next/image';
 import Link from 'next/link';
-import { Tabs, TabsList, TabsTrigger } from '@shotly/ui/components/tabs';
-import { Input } from '@shotly/ui/components/input';
-import Empty from '../../empty';
 
 async function CollectionDetails({
   params,
@@ -28,51 +24,6 @@ async function CollectionDetails({
 
   return (
     <div className="h-full flex flex-col">
-      <MainHeader>
-        <Link
-          href="/portfolio"
-          className={cn(
-            buttonVariants({ size: 'icon', variant: 'ghost' }),
-            'size-7 mr-3',
-          )}
-        >
-          <ChevronLeft />
-        </Link>
-        <div>
-          <h2 className="font-medium">Karra Loft</h2>
-          <div className="text-xs space-x-2 text-muted-foreground">
-            Jun 19th 2023
-          </div>
-        </div>
-        <div className="space-x-3 ml-auto flex items-center">
-          <Badge
-            variant="outline"
-            className="text-sm rounded-full px-3 py-1 border"
-          >
-            <span
-              className={cn(
-                'size-1 rounded-full',
-                isPublic ? 'bg-green-400' : 'bg-destructive',
-              )}
-            >
-              &nbsp;
-            </span>
-            {isPublic ? (
-              <>
-                <Globe /> Public
-              </>
-            ) : (
-              <>
-                <Lock /> Private
-              </>
-            )}
-          </Badge>
-          <Button variant="outline">Edit</Button>
-          <Button>
-            <UploadIcon /> Upload photos
-          </Button>
-        </div>
-      </MainHeader>
       <div className="p-3 space-y-6">
         <div className="w-full h-60 relative">
           <Image
@@ -82,51 +33,69 @@ async function CollectionDetails({
             height={200}
             alt="Cover image of the collection"
           />
-          <div className="absolute bottom-5 left-5">
-            <h3 className=" font-bold text-xl text-background">Karra Loft</h3>
-            <p className="text-sm text-background">Canada, Vancouver</p>
-            <div className="flex">
-              <p className="flex space-x-2 text-xs text-background mt-2">
+          <Link
+            href="/portfolio"
+            className={cn(
+              buttonVariants({ size: 'icon', variant: 'outline' }),
+              'size-7 absolute left-5 top-5 rounded-full',
+            )}
+          >
+            <ChevronLeft />
+          </Link>
+          <Button
+            size="icon"
+            variant="outline"
+            className={cn('size-7 absolute right-5 top-5 rounded-full')}
+          >
+            <EllipsisIcon />
+          </Button>
+        </div>
+        <div className="flex justify-between items-start px-3">
+          <div>
+            <h3 className=" font-bold text-2xl mb-1">Summer Travel 2023</h3>
+            <p className="text-sm text-muted-foreground mb-3">
+              Photos from our European journey through Italy and France
+            </p>
+            <div className="flex space-x-4 text-muted-foreground mb-3">
+              <p className="flex space-x-2 text-xs mt-2">
                 <ImagesIcon className="size-4" /> <span>{10} assets</span>
+              </p>
+              <p className="flex space-x-2 text-xs mt-2">
+                <MapPinIcon className="size-4" /> <span>Kyiv, Ukraine</span>
+              </p>
+              <p className="flex space-x-2 text-xs mt-2">
+                <CalendarIcon className="size-4" /> <span>Aug 15, 2023</span>
+              </p>
+              <p className="flex space-x-2 text-xs mt-2">
+                <TagIcon className="size-4" /> <span>Landspace</span>
               </p>
             </div>
           </div>
-        </div>
-        <div className="flex space-x-4 px-5">
-          <div className="items-center space-x-2">
-            <p className="flex items-center space-x-2 text-muted-foreground text-xs">
-              Location
-            </p>
-            <p className="text-sm">Kyiv, Ukraine</p>
+          <div className="space-y-1">
+            <Badge
+              variant="outline"
+              className="text-sm rounded-full px-3 py-1 border"
+            >
+              <span
+                className={cn(
+                  'size-1 rounded-full',
+                  isPublic ? 'bg-green-400' : 'bg-destructive',
+                )}
+              >
+                &nbsp;
+              </span>
+              {isPublic ? (
+                <>
+                  <Globe /> Public
+                </>
+              ) : (
+                <>
+                  <Lock /> Private
+                </>
+              )}
+            </Badge>
           </div>
-          <div className="items-center space-x-2 px-5">
-            <p className="flex items-center text-muted-foreground text-xs">
-              Category
-            </p>
-            <p className="text-sm">Landspace</p>
-          </div>
-          <div className="ml-auto items-center space-x-2">
-            <p className="flex items-center text-muted-foreground text-xs">
-              Views
-            </p>
-            <p className="text-sm">1,109</p>
-          </div>
         </div>
-        <Separator />
-        <div className="flex space-x-3 justify-end">
-          <Input placeholder="Search photos..." />
-          <Tabs defaultValue="account">
-            <TabsList>
-              <TabsTrigger value="account">
-                <GridIcon />
-              </TabsTrigger>
-              <TabsTrigger value="password">
-                <ListIcon />
-              </TabsTrigger>
-            </TabsList>
-          </Tabs>
-        </div>
-        <Empty />
       </div>
     </div>
   );
