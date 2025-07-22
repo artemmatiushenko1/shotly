9abb8e28a5640dd7765190ca611e46d3e267b1cc
artemmatiushenko1 <artom.matyushenko@gmail.com>
2025-07-22 08:37:00 +0300

    remove sidebar trigger

--- apps/web/src/app/(photographer)/portfolio/page.tsx
+++ apps/web/src/app/(photographer)/portfolio/page.tsx
@@ -31,7 +31,7 @@ function Portfolio() {
         )}
       </MainHeader>
       <div className="p-2 pt-4 space-y-3">
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
           {cardsData.map((collection) => (
             <Link
               key={collection.id}
--- apps/web/src/components/main-header.tsx
+++ apps/web/src/components/main-header.tsx
@@ -1,15 +1,10 @@
-import { Separator } from '@shotly/ui/components/separator';
-import { SidebarTrigger } from '@shotly/ui/components/sidebar';
-
 type MainHeadrProps = {
   children: React.ReactNode;
 };
 
 function MainHeader({ children }: MainHeadrProps) {
   return (
-    <div className="text-md border-b -ml-2 -mr-2 p-4 flex items-center gap-3 sticky top-0 bg-background z-10">
-      <SidebarTrigger />
-      <Separator orientation="vertical" />
+    <div className="text-md p-4 flex items-center gap-3 sticky top-0 bg-background z-10">
       <div className="flex w-full items-center">{children}</div>
     </div>
   );
