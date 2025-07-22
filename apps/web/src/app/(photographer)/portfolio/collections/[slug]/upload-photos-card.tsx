e60a47492e5aec5e900d9d97cb3bcee4d3038c01
artemmatiushenko1 <artom.matyushenko@gmail.com>
2025-07-22 22:08:03 +0300

    remove background

--- apps/web/src/app/(photographer)/portfolio/collections/[slug]/upload-photos-card.tsx
+++ apps/web/src/app/(photographer)/portfolio/collections/[slug]/upload-photos-card.tsx
@@ -4,7 +4,7 @@ import { UploadIcon } from 'lucide-react';
 const UploadPhotosCard = () => {
   return (
     <Card className="overflow-hidden cursor-pointer hover:shadow-lg transition-shadow border-2 border-dashed border-accent hover:border-primary h-48 p-0 shadow-none">
-      <div className="relative h-48 flex flex-col items-center justify-center bg-gray-50 hover:bg-primary/5 transition-colors">
+      <div className="relative h-48 flex flex-col items-center justify-center hover:bg-primary/5 transition-colors">
         <UploadIcon className="size-8 mb-2 text-muted-foreground" />
         <p className="text-sm font-medium text-muted-foreground">
           Upload Photos
