863b5e1e3126343fcbf57334e5c7277c63b64fe7
artemmatiushenko1 <artom.matyushenko@gmail.com>
2025-10-18 15:23:31 +0200

    configure eslint

--- apps/web/eslint.config.mjs
+++ apps/web/eslint.config.mjs
@@ -1,16 +1,4 @@
-import { dirname } from 'path';
-import { fileURLToPath } from 'url';
-import { FlatCompat } from '@eslint/eslintrc';
+import { nextJsConfig } from "@shotly/eslint-config/next-js"
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
-
-const eslintConfig = [
-  ...compat.extends('next/core-web-vitals', 'next/typescript', 'prettier'),
-];
-
-export default eslintConfig;
+/** @type {import("eslint").Linter.Config} */
+export default nextJsConfig
---
+++ packages/ui/eslint.config.js
@@ -0,0 +1,4 @@
+import { config } from "@shotly/eslint-config/react-internal"
+
+/** @type {import("eslint").Linter.Config} */
+export default config
