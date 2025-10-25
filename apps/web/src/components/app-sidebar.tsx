2592a5156ed6bf2241d76f5e5eaee6ac4ba5592c
artemmatiushenko1 <artom.matyushenko@gmail.com>
2025-10-25 17:27:19 +0200

    display base info on public profile

--- apps/web/src/components/app-sidebar.tsx
+++ apps/web/src/components/app-sidebar.tsx
@@ -91,7 +91,7 @@ export function AppSidebar(props: AppSidebarProps) {
   return (
     <Sidebar variant="inset">
       <SidebarHeader className="pt-1">
-        <div className="flex gap-3 items-center justify-center rounded-md bg-gradient-to-r from-primary to-[#005DDB]">
+        <div className="flex gap-3 items-center justify-center rounded-md bg-gradient-to-r from-orange-300 via-pink-300 to-purple-300">
           <Logo />
         </div>
       </SidebarHeader>
