4bc033a20ba955c61ae3db82ccb5adf1661c1e20
artemmatiushenko1 <artom.matyushenko@gmail.com>
2025-11-06 21:08:51 +0100

    fix link

--- apps/web/src/app/(photographer)/portfolio/use-cases/see-collections/collections-grid.tsx
+++ apps/web/src/app/(photographer)/portfolio/use-cases/see-collections/collections-grid.tsx
@@ -22,10 +22,7 @@ const CollectionsGrid = ({ collections }: CollectionsGridProps) => {
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 p-3 pt-4">
       {collections.map((collection) => (
-        <Link
-          key={collection.id}
-          href={`/portfolio/collections/${collection.id}`}
-        >
+        <Link key={collection.id} href={`/portfolio/${collection.id}`}>
           <CollectionCard
             isPublic={collection.isPublic}
             title={collection.title}
