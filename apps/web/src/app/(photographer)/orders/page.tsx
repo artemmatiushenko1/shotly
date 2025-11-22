ee490b41be5a0e1724566053f7b7174104d9b8e2
artemmatiushenko1 <artom.matyushenko@gmail.com>
2025-11-22 22:26:04 +0100

    add orders route

---
+++ apps/web/src/app/(photographer)/orders/page.tsx
@@ -0,0 +1,11 @@
+import MainHeader from '@/components/main-header';
+
+function Orders() {
+  return (
+    <div>
+      <MainHeader title="Orders" caption="Manage your orders" />
+    </div>
+  );
+}
+
+export default Orders;
