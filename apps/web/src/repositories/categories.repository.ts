d4c286400c6726b6b1964aa89eab2276d8d710c8
artemmatiushenko1 <artom.matyushenko@gmail.com>
2025-11-30 13:21:21 +0100

    remove getCategoryById

--- apps/web/src/repositories/categories.repository.ts
+++ apps/web/src/repositories/categories.repository.ts
@@ -1,26 +1,11 @@
 import { db } from '@/db/drizzle';
 import { categoriesTable } from '@/db/schema';
 import { Category, categorySchema } from '@/domain/category';
-import { eq } from 'drizzle-orm';
 
 class CategoriesRepository {
   async getCategories(): Promise<Category[]> {
     const categories = await db.select().from(categoriesTable);
     return categories.map((category) => categorySchema.parse(category));
-  }
-
-  async getCategoryById(id: string): Promise<Category> {
-    const [category] = await db
-      .select()
-      .from(categoriesTable)
-      .where(eq(categoriesTable.id, id));
-
-    if (!category) {
-      // TODO: throw NotFoundError
-      throw new Error('Category not found.');
-    }
-
-    return categorySchema.parse(category);
   }
 }
 
